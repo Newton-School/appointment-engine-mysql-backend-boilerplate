f258be2b7bd1b73b974ba39e6af6ac976159e28b
EdOates84 <niteshmahawer1@gmail.com>
2021-02-19 18:12:14 +0530

    fix boilerplate

--- src/createdatabase.js
+++ src/createdatabase.js
@@ -1,6 +1,6 @@
 
-const connection = require("./connector");
-const { data } = require('./data')
+const con = require("./connector");
+const { slot_data,data } = require('./data')
 
 const refreshAll = async () => {
 
@@ -11,34 +11,37 @@ const refreshAll = async () => {
             }
         });
 
-    await con.query("CREATE TABLE events (id INT AUTO_INCREMENT NOT NULL, date DATE NOT NULL,time TIME NOT NULL, book TINYINT(0) DEFAULT 0, primary key (id));",
+    await con.query("CREATE TABLE events (id INT AUTO_INCREMENT NOT NULL, date DATE NOT NULL,slot1 INT, slot2 INT, slot3 INT, slot4 INT, slot5 INT, slot6 INT, slot7 INT, slot8 INT, slot9 INT, slot10 INT,slot11 INT, slot12 INT, slot13 INT, slot14 INT, primary key (id));",
         (err, result) => {
             if (err) {
                 console.log(err)
             }
         });
 
-    await con.query("CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, slot_id INT NOT NULL, name TEXT(65535), primary key (id));",
+    await con.query("CREATE TABLE user (id INT AUTO_INCREMENT NOT NULL, name TEXT(65535), primary key (id));",
     (err, result) => {
         if (err) {
             console.log(err)
         }
     });
-
+    var i = 1;
     await slot_data.forEach(element => {
-        con.query("INSERT INTO events (date, time, book) VALUES (?,?,?);", [element.date, element.time, element.book], (err, result) => {
+        i++;
+        con.query("INSERT INTO events VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?);", [i].concat(Object.values(element)), (err, result) => {
             if (err) {
                 console.log(err)
             }
         })
     });
-
+    i = 1;
     await data.forEach(element => {
-        con.query("INSERT INTO user (name, slot_id) VALUES (?,?);", [element.name, element.slot_id], (err, result) => {
+        i++;
+        con.query("INSERT INTO user  VALUES (?,?);",[i].concat(Object.values(element)), (err, result) => {
             if (err) {
                 console.log(err)
             }
         })
     });
+    console.log("data refreshed");
 }
-refreshAll()
+refreshAll();
